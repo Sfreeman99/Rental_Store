b336a8ed75af0513adf6636a0a994a2190b26a5a
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 11:22:47 -0500

    [shell][status: works] edited so the program would look bette; [test: passed] added new test which passed

--- shell.py
+++ shell.py
@@ -20,8 +20,7 @@ def main():
 def rent_atv():
     #the variable 'how many' is used to see how many of the products the customer wants
     product_name = 'Atv'.strip()
-    how_many = int(input('how many Atv\'s would you like?'))
-    # rental_products['Atv']['stock'] -= how_many
+    how_many = int(input('how many Atv\'s would you like?\n:'))
     remove_stock(how_many,product_name,rental_products)
     print(rental_products)
     
@@ -29,8 +28,9 @@ def rent_atv():
 #sbs stands for side-by-side
 def rent_sbs():
     product_name = 'Side-by-Side'.strip()
-    how_many = int(input('how many Side-by-Side\'s would you like?'))
-    rental_products['Side-by-Side']['stock'] -= how_many
+    how_many = int(input('how many Side-by-Side\'s would you like?\n:'))
+    # rental_products['Side-by-Side']['stock'] -= how_many
+    remove_stock(how_many, product_name, rental_products)
     print(rental_products)
     
 #if they choose the number 3 they get both an atv and side-by-side
--- test_core.py
+++ test_core.py
@@ -7,3 +7,16 @@ def test_remove_one_atv():
 def test_remove_atv_with_more_than_one_dict():
     assert (remove_stock(1, 'Atv', {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price': 100, 'stock': 5}}) ==
             {'Atv':{'price': 30, 'stock': 4}, 'Side-by-Side':{'price':100, 'stock': 5}})
+
+def test_remove_one_sbs():
+    assert (remove_stock(1, 'Side-by-Side', {'Side-by-Side':{'price': 30, 'stock': 5}}) ==
+            {'Side-by-Side': {'price': 30, 'stock': 4}})
+    
+
+
+
+
+def test_remove_sbs_with_more_than_one_dict():
+    assert (remove_stock(1, 'Side-by-Side', {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price': 100, 'stock': 5}}) ==
+            {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price':100, 'stock': 4}})
+    
