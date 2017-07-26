e8ec63974c5a8972328a2b269f859000fa119566
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 11:04:23 -0500

    [shell -> core][status: working] made a function in core to work with hard coded dictionary

--- core.py
+++ core.py
@@ -1,1 +1,27 @@
 rental_products = {'Atv':{'price': 30, 'stock': 1}, 'Side-by-Side':{'price': 100, 'stock': 1}}
+
+
+def remove_stock(how_many,product_name, rental_products):
+    ''' (int, dict of dict) -> 
+
+    takes in how many of the products the customer wants
+    and takes it away from stock
+
+    >>> (remove_stock(3,'Atv',{'Atv':{'price': 30, 'stock': 5}}) ==
+    ... {'Atv':{'price': 30, 'stock': 2}})
+    True
+    >>> (remove_stock(5,'Atv',{'Atv':{'price': 30, 'stock': 5}}) ==
+    ... {'Atv':{'price': 30, 'stock': 0}})
+    True  
+    >>> (remove_stock(1,'Atv',{'Atv':{'price': 30, 'stock': 5}}) ==
+    ... {'Atv':{'price': 30, 'stock': 4}})
+    True
+    '''
+
+    if product_name == 'Atv':
+        rental_products[product_name]['stock'] -= how_many
+    if product_name == 'Side-by-Side':
+        rental_products[product_name]['stock'] -= how_many
+    return rental_products
+        
+    
--- shell.py
+++ shell.py
@@ -1,4 +1,4 @@
-from core import rental_products
+from core import rental_products, remove_stock
 
 def main():
     print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
@@ -19,22 +19,24 @@ def main():
 
 def rent_atv():
     #the variable 'how many' is used to see how many of the products the customer wants
+    product_name = 'Atv'.strip()
     how_many = int(input('how many Atv\'s would you like?'))
-    rental_products['Atv']['stock'] -= how_many
+    # rental_products['Atv']['stock'] -= how_many
+    remove_stock(how_many,product_name,rental_products)
     print(rental_products)
     
 #if they choose the number 2 they get a side-by-side
 #sbs stands for side-by-side
 def rent_sbs():
+    product_name = 'Side-by-Side'.strip()
     how_many = int(input('how many Side-by-Side\'s would you like?'))
     rental_products['Side-by-Side']['stock'] -= how_many
     print(rental_products)
     
 #if they choose the number 3 they get both an atv and side-by-side
 def rent_both():
-    print('Both')
-    how_many_atv = int(input('how many Atv\'s would you like?'))
-    how_many_sbs = int(input('how many Side-by-Side\'s would you like?'))    
+    product_name = input('Atv or Side-by-Side').title().strip()
+    how_many = int(input('how many',product_name,' would you like?'))  
     rental_products['Atv']['stock'] -= how_many_atv
     rental_products['Side-by-Side']['stock'] -= how_many_sbs
     print(rental_products)
