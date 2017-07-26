f2ed14076cbd7f9bc1b31c3ebfdabbe6cfde87e7
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 09:27:58 -0500

    [shell] added hard coded dictionary

--- shell.py
+++ shell.py
@@ -1,3 +1,5 @@
+rental_products = {'Atv':{'price': 30, 'stock': 1}, 'Side-by-Side':{'price': 100, 'stock': 1}}
+
 print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
 # give the user choices through numbers So it can be easier to choose what you want
 decision = input(''' What would you like:
@@ -5,6 +7,13 @@ decision = input(''' What would you like:
 \t2. Side-by-Sides\n:''')
 
 if decision == '1':
-    print('Atvs')
+    print('Atv')
+    rental_products['Atv']['stock'] -= 1
 elif decision == '2':
-    print('Side-by-Sides')
+    print('Side-by-Side')
+    rental_products['Side-by-Side']['stock'] -= 1
+
+print(rental_products)
+
+
+
