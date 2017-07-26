1005f1bca974fba1ec616f3120a62a6ad89e91d9
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 11:12:39 -0500

    [test: passed] added test for remove from stock

---
+++ test_core.py
@@ -0,0 +1,9 @@
+from core import *
+
+def test_remove_one_atv():
+    assert (remove_stock(1, 'Atv', {'Atv':{'price': 30, 'stock': 5}}) ==
+            {'Atv': {'price': 30, 'stock': 4}})
+
+def test_remove_atv_with_more_than_one_dict():
+    assert (remove_stock(1, 'Atv', {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price': 100, 'stock': 5}}) ==
+            {'Atv':{'price': 30, 'stock': 4}, 'Side-by-Side':{'price':100, 'stock': 5}})
