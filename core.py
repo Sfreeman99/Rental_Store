62561366db3638e9502ed4ded16c1aa4fe5417e5
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 13:58:08 -0500

    [shell] knocked down some code; [core] addednew function

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ rental_products = {'Atv':{'price': 30, 'stock': 1}, 'Side-by-Side':{'price': 100
 
 
 def remove_stock(how_many,product_name, rental_products):
-    ''' (int, dict of dict) -> 
+    ''' (int,str, dict of dict) -> 
 
     takes in how many of the products the customer wants
     and takes it away from stock
@@ -24,4 +24,23 @@ def remove_stock(how_many,product_name, rental_products):
         rental_products[product_name]['stock'] -= how_many
     return rental_products
         
+
+def total(how_many,product_name, rental_products):
+    ''' int,str, dict of dict -> total
+
+    returns the total transaction of what you rented
+
+    >>> total(1, 'Atv', {'Atv':{'price': 30, 'stock': 3}})
+    30.07
+    >>> total(2, 'Atv', {'Atv':{'price': 30, 'stock': 3}})
+    60.14
+    >>> total(0, 'Atv', {'Atv':{'price': 30, 'stock': 3}})
+    0.0
+    >>> total(1, 'Atv', {'Atv':{'price': 30, 'stock': 3}})
+    30.07
+    '''
+    sales_tax = .07 * how_many
+    price = (rental_products[product_name]['price'] * how_many)
+    total = price + sales_tax
+    return total
     
--- shell.py
+++ shell.py
@@ -1,49 +1,45 @@
-from core import rental_products, remove_stock
+from core import rental_products, remove_stock, total
 
 def main():
     print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
     # give the user choices through numbers So it can be easier to choose what you want
     decision = input(''' What would you like:
-    \t1. Atvs
-    \t2. Side-by-Sides
-    \t3. Both
+    \t1. Rent
+    \t2. Return
     :''').strip()
     #user chooses through numbers what they want
     #if they choose the number 1 they get an atv in stock
     if decision == '1':
-        rent_atv()
+        rent()
     elif decision == '2':
-        rent_sbs()
-    elif decision == '3':
-        rent_both()
+        return_item()
 
-def rent_atv():
+def rent():
     #the variable 'how many' is used to see how many of the products the customer wants
-    product_name = 'Atv'.strip()
-    how_many = int(input('how many Atv\'s would you like?\n:'))
-    sales_tax = .07 * how_many
-    price = (rental_products[product_name]['price'] * how_many)
-    total = price + sales_tax
-    remove_stock(how_many,product_name,rental_products)
-    print(rental_products)
-    print('$:',total)
+    decision = input('What do you want to rent:\n\t1. Atv\n\t2. Side-by-Side\n:')
+    
+    if decision == '1':
+        product_name = 'Atv'.strip()
+    
+        how_many = int(input('how many Atv\'s would you like?\n:'))
+    
+        remove_stock(how_many,product_name,rental_products)
+    
+        print('$:',total(how_many,product_name,rental_products))
+    
+    if decision == '2':
+        product_name = 'Side-by-Side'.strip()
+    
+        how_many = int(input('how many Atv\'s would you like?\n:'))
+    
+        remove_stock(how_many,product_name,rental_products)
+
+        print('$:',total(how_many,product_name,rental_products))        
     
 #if they choose the number 2 they get a side-by-side
 #sbs stands for side-by-side
-def rent_sbs():
-    product_name = 'Side-by-Side'.strip()
-    how_many = int(input('how many Side-by-Side\'s would you like?\n:'))
-    sales_tax = .07 * how_many
-    price = (rental_products[product_name]['price'] * how_many)
-    total = price + sales_tax    
-    remove_stock(how_many, product_name, rental_products)
-    print(rental_products)
-    print('$:',total)
+
     
-#if they choose the number 3 they get both an atv and side-by-side
-def rent_both():
-    rent_atv()
-    rent_sbs()
 if __name__ == '__main__':
     main()
 
