90f5b76b1674f7d944fdf1235c493701c00a6600
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 09:59:45 -0500

    [shell][status: Works] added variables so you can rent both atvs and side-by-sides

--- shell.py
+++ shell.py
@@ -18,8 +18,10 @@ elif decision == '2':
 #if they choose the number 3 they get both an atv and side-by-side
 elif decision == '3':
     print('Both')
-    rental_products['Atv']['stock'] -= 1
-    rental_products['Side-by-Side']['stock'] -= 1
+    how_many_atv = int(input('how many Atv\'s would you like?'))
+    how_many_sbs = int(input('how many Side-by-Side\'s would you like?'))    
+    rental_products['Atv']['stock'] -= how_many_atv
+    rental_products['Side-by-Side']['stock'] -= how_many_sbs
 
 print(rental_products)
 
