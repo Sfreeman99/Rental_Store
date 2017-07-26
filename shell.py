02f7dc77b341a7083cb4089b5990ef891c45a318
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 09:18:42 -0500

    [shell] added a decision to choose two different products

--- shell.py
+++ shell.py
@@ -1,1 +1,10 @@
 print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
+# give the user choices through numbers So it can be easier to choose what you want
+decision = input(''' What would you like:
+\t1. Atvs
+\t2. Side-by-Sides\n:''')
+
+if decision == '1':
+    print('Atvs')
+elif decision == '2':
+    print('Side-by-Sides')
