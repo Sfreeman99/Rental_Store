a5223bd0fdae7208096b8bd12f42c9a77ecfe707
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-26 09:53:34 -0500

    [shell][status: Works] added a new variable 'how_many' so that the customer can tell you how many of that product you want

--- shell.py
+++ shell.py
@@ -5,15 +5,17 @@ decision = input(''' What would you like:
 \t1. Atvs
 \t2. Side-by-Sides
 \t3. Both
-:''')
-
+:''').strip()
+#user chooses through numbers what they want
+#if they choose the number 1 they get an atv in stock
 if decision == '1':
-    print('Atv')
-    rental_products['Atv']['stock'] -= 1
+    how_many = int(input('how many Atv\'s would you like?'))
+    rental_products['Atv']['stock'] -= how_many
+#if they choose the number 2 they get a side-by-side
 elif decision == '2':
-    print('Side-by-Side')
-    rental_products['Side-by-Side']['stock'] -= 1
-
+    how_many = int(input('how many Side-by-Side\'s would you like?'))
+    rental_products['Side-by-Side']['stock'] -= how_many
+#if they choose the number 3 they get both an atv and side-by-side
 elif decision == '3':
     print('Both')
     rental_products['Atv']['stock'] -= 1
