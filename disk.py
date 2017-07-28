3643db89f7b5cd8bafc558b55c11376730225436
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-28 13:31:40 -0500

    fixed a problem where the inventory would not be read as a dictionary
    what i plan to do next is to fix the shell input so that it fits the dictionary made in disk

--- disk.py
+++ disk.py
@@ -6,20 +6,17 @@ def read_inventory():
     rental_products = {}
     for item in inventory:
         sublist = item.strip().split('||')
-        # rental_products[sublist[0].strip()] = {
-        #     'Year': int(sublist[1].strip()),
-        #     'Manufacturer': sublist[2].strip(),
-        #     'Original Price': float(sublist[3].strip()),
-        #     'stock': int(sublist[4].strip()),
-        #     'rental price': float(sublist[5].strip())
-        # }
-        rental_products[sublist[0].strip()] = rental_products[sublist[0].strip()].update({sublist[2].strip(): {}})
+        rental_products[sublist[0].strip()] = {
+            'Original Price': float(sublist[1].strip()),
+            'stock': int(sublist[2].strip()),
+            'rental price': float(sublist[3].strip())
+        }
 
     print(rental_products)
     
 def write_inventory(new_inventory):
     message = 'Category || Year || Manufacturer || Original Price || In Stock || Price to Rent ||\n'
     for key, value in new_inventory.items():
-        message += '{} || {} || {} || {} || {} || {}\n'.format(key, value['Year'], value['Manufacturer'], value['Original Price'],value['stock'], value['rental price'])
+        message += '{} || {} || {} || {}\n'.format(key, value['Original Price'],value['stock'], value['rental price'])
     with open('inventory.txt','w') as new_file:
         new_file.write(message)
