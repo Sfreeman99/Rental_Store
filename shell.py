685715ccfaad4d7b4d99e6c57f12749609b9af56
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-07-31 15:30:05 -0500

    added the customer and employee mains and fixed code up in the shell
    i plan to expand the employee shell

--- shell.py
+++ shell.py
@@ -1,17 +1,17 @@
 from core import *
 from disk import *
-def main():
+def customer_main():
     print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
     # give the user choices through numbers So it can be easier to choose what you want
     decision = input(''' What would you like:
     \t1. Rent
     \t2. Return
-    :''').strip()
+    :''').title().strip()
     #user chooses through numbers what they want
     #if they choose the number 1 they get an atv in stock
-    if decision == '1':
+    if decision == 'Rent':
         rent()
-    elif decision == '2':
+    elif decision == 'Return':
         return_item()
 
 def rent():
@@ -45,20 +45,47 @@ def rent():
 
 def return_item():
     rental_products = read_inventory()
-    name = input('What is your name?').title()
-    category = input('What do you want to rent:\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
-    item = input('What do you want to return:\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
-    add_stock(product_name, rental_products)
+    name = input('What is your name?\n:').title()
+    category = input('What do you want to return:\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
+    new_inventory = add_stock(category, rental_products)
+    write_inventory(new_inventory)
     current_deposit = deposit(category,rental_products)
-    print('Here is your deposit of', current_deposit,'back.\n\tHave A Nice Day !!!')
+    print('Here is your deposit of ${:.2f} back {}.\n\tHave A Nice Day !!!'.format(current_deposit, name))
     
 
 
         
 
 #if they choose the number 2 they get a side-by-side
-#sbs stands for side-by-side
-    
+#sbs stands for side-by-side  
+def main():
+    print('Welcome to Shedlia\'s Atv and Side-By-Side Rental Agency')
+    answer = ''
+    while answer != 'customer' or 'employee':
+        answer = input('Are you a customer or employee\n:').lower()
+        if answer == 'customer':
+            customer_main()
+            break
+        elif answer == 'employee':
+            employee_main()
+            break
+        else:
+            print('please choose one or the other and spell it correctly...')
+
+def employee_main():
+    print('Welcome employee of Shedlia\'s Rental Store')
+    decision = ''
+    while decision != '1' or '2' or '3':
+        decision = input('What do you want to do today?\n\t1. Add product to stock\n\t2. Check total revenue\n\t3.See transaction History\n\t**Pick numbers Only**\n:')
+        if decision == '1':
+            print('1 works')
+            break
+        elif decision == '2':
+            print('2 works')
+            break
+        elif decision == '3':
+            print('3 works')
+            break
 if __name__ == '__main__':
     main()
 
