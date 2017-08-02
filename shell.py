2830aaa9863274e8b47f22511dc7a6d98aaa935c
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-08-02 09:47:39 -0500

    added more while loops to my shell

--- shell.py
+++ shell.py
@@ -11,39 +11,93 @@ def customer_main():
         :''').title().strip()
         if decision == 'Rent':
             rent()
+            break
         elif decision == 'Return':
             return_item()
+            break
         else:
             print('Invalid Choice... Please type Rent or Return\n')
 
 def rent():
     #the variable 'how many' is used to see how many of the products the customer wants
-    name = input('What is your first and last name?\n:').title()
-    category = input('What do you want to rent:\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
     rental_products = read_inventory()
-    if category in rental_products:
+    name = input('What is your first and last name?\n:').title()
+    category = category_decision()
+    # while category != 'Atv' or 'Side-by-Side' or 'Scooter' or 'Bike':
+    #     category = input('What do you want to rent:\n\t**Spell Exactly how it is shown**\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
+    #     if category in rental_products:
         
-        how_many_hours = int(input('how many hours would you like?\n:'.format(category)))
+    # how_many_hours = int(input('how many hours would you like?\n:'.format(category)))
 
-        current_deposit = deposit(category,rental_products)
+    how_many_hours = how_many_hours_decision(category, name)
+
+    current_deposit = deposit(category,rental_products)
+
+    new_inventory = remove_stock(category,rental_products)
+
+    write_inventory(new_inventory)
+
+    total_amount = total(how_many_hours, category, rental_products)
+
+    print('Your deposit would be $:',round(current_deposit, 2), sep = '')
+
+    print('Your total will be $:',total_amount, sep = '')
+
+    print('With tax added that would be $:', round(sales_tax(total_amount), 3), sep = '')
+
+    decision = 'renting'
+
+    tax = sales_tax(total_amount)
+
+    history(name, category, decision, tax)
+
     
-        new_inventory = remove_stock(category,rental_products)
 
-        write_inventory(new_inventory)
+        # else:
+        #     print('Invalid Choice...\n')
 
-        total_amount = total(how_many_hours, category, rental_products)
+    # if category in rental_products:
+        
+    #     how_many_hours = int(input('how many hours would you like?\n:'.format(category)))
 
-        print('Your deposit would be $:',round(current_deposit, 2), sep = '')
+    #     current_deposit = deposit(category,rental_products)
     
-        print('Your total will be $:',total_amount, sep = '')
+    #     new_inventory = remove_stock(category,rental_products)
 
-        print('With tax added that would be $:', round(sales_tax(total_amount), 3), sep = '')
+    #     write_inventory(new_inventory)
 
-        decision = 'renting'
+    #     total_amount = total(how_many_hours, category, rental_products)
 
-        tax = sales_tax(total_amount)
+    #     print('Your deposit would be $:',round(current_deposit, 2), sep = '')
+    
+    #     print('Your total will be $:',total_amount, sep = '')
 
-        history(name, category, decision, tax)
+    #     print('With tax added that would be $:', round(sales_tax(total_amount), 3), sep = '')
+
+    #     decision = 'renting'
+
+    #     tax = sales_tax(total_amount)
+
+    #     history(name, category, decision, tax)
+def how_many_hours_decision(category, name):
+    how_long = ''
+    while True:
+        how_long = int(input('How long would you like to rent the {} {}\n1hr\n2hrs\n3hrs\n4hrs?'.format(category, name)))
+        if how_long == 1 or 2 or 3 or 4:
+            return how_long
+            break
+        else:
+            print('Invalid Choice of string... Please enter an Integar')
+def category_decision():
+    rental_products = read_inventory()
+    category = ''
+    while True:
+        category = input('What do you want to rent:\n\t**Spell Exactly how it is shown**\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:')
+        if category in rental_products:
+            return category
+            break
+        else:
+            print('Invalid Choice.... Please Try Again\n')
 
 def return_item():
     rental_products = read_inventory()
@@ -58,7 +112,7 @@ def main():
     print('Welcome to Shedlia\'s Wheels 4 You')
     answer = ''
     while answer != 'customer' or 'employee':
-        answer = input('Are you a customer or employee\n:').lower()
+        answer = input('Are you a customer or employee\n:').lower().strip()
         if answer == 'customer':
             customer_main()
             break
@@ -93,6 +147,10 @@ def employee_main():
         else:
             print('Not Valid Input... Please Try Again!!!')
             
+
+
+
+
 if __name__ == '__main__':
     main()
 
