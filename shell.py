19afe0155544b2ee74d4d4d4deceada12a436d79
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-08-08 09:56:39 -0500

    changed my importing from import all to importing individual functions

--- shell.py
+++ shell.py
@@ -1,5 +1,7 @@
-from core import *
-from disk import *
+import core
+import disk
+
+
 def customer_main():
     print('Welcome Customer')
     decision = ''
@@ -18,65 +20,79 @@ def customer_main():
         else:
             print('Invalid Choice... Please type Rent or Return\n')
 
+
 def rent():
     decision = 'rent'
-    rental_products = read_inventory()
-    current_menu = menu(rental_products)
+    rental_products = disk.read_inventory()
+    current_menu = disk.menu(rental_products)
     name = input('What is your first and last name?\n:').title()
     category = category_decision(decision, current_menu)
 
-    current_deposit = deposit(category,rental_products)    
+    current_deposit = core.deposit(category, rental_products)
 
-    print('Your deposit would be $:{:.2f}'.format(current_deposit))    
+    print('Your deposit would be $:{:.2f}'.format(current_deposit))
 
     how_many_hours = how_many_hours_decision(category, name)
 
-    new_inventory = remove_stock(category,rental_products)
+    new_inventory = core.remove_stock(category, rental_products)
 
-    write_inventory(new_inventory)
+    disk.write_inventory(new_inventory)
 
-    total_amount = total(how_many_hours, category, rental_products)
+    total_amount = core.total(how_many_hours, category, rental_products)
 
     print('Your total will be $: {:.2f}'.format(total_amount))
 
-    print('With tax added that would be $: {:.2f}'.format(sales_tax(total_amount)))
+    print('With tax added that would be $: {:.2f}'.format(
+        core.sales_tax(total_amount)))
 
-    tax = sales_tax(total_amount)
+    tax = core.sales_tax(total_amount)
 
-    history(name, category, decision, tax)
+    disk.history(name, category, decision, tax)
+
 
 def how_many_hours_decision(category, name):
     how_long = ''
     while True:
-        how_long = input('How long would you like to rent the {} {}\n\t1. 1hr\n\t2. 2hrs\n\t3. 3hrs\n\t4. 4hrs?\n:'.format(category, name))
-        if (how_long == '1') or (how_long == '2') or (how_long == '3') or (how_long == '4'):
+        how_long = input(
+            'How long would you like to rent the {} {}\n\t1. 1hr\n\t2. 2hrs\n\t3. 3hrs\n\t4. 4hrs?\n:'.
+            format(category, name))
+        if (how_long == '1') or (how_long == '2') or (how_long == '3') or (
+                how_long == '4'):
             return int(how_long)
             break
         else:
             print('Invalid Choice... Please choose 1, 2, 3, or 4\n')
+
+
 def category_decision(decision, current_menu):
-    rental_products = read_inventory()
+    rental_products = disk.read_inventory()
     category = ''
     while True:
         # category = input('What do you want to {}:\n\t**Spell Exactly how it is shown**\n\t1. Atv\n\t2. Side-by-Side\n\t3. Scooter\n\t4. Bike\n:'.format(decision)).strip()
-        category = input('What do you want to {}:\n\t**Spell Exactly how it is shown**\n{}'.format(decision, current_menu)).strip()
-        
+        category = input(
+            'What do you want to {}:\n\t**Spell Exactly how it is shown**\n{}\n:'.
+            format(decision, current_menu)).strip()
+
         if category in rental_products:
             return category
             break
         else:
             print('Invalid Choice.... Please Try Again\n')
 
+
 def return_item():
     decision = 'return'
-    rental_products = read_inventory()
+    rental_products = disk.read_inventory()
     name = input('What is your name?\n:').title().strip()
     category = category_decision(decision)
-    new_inventory = add_stock(category, rental_products)
-    write_inventory(new_inventory)
-    current_deposit = deposit(category,rental_products)
-    print('Here is your deposit of ${:.2f} back {}.\n\tHave A Nice Day !!!'.format(current_deposit, name))
-  
+    new_inventory = core.add_stock(category, rental_products)
+    disk.write_inventory(new_inventory)
+    current_deposit = core.deposit(category, rental_products)
+    print('Here is your deposit of ${:.2f} back {}.\n\tHave A Nice Day !!!'.
+          format(current_deposit, name))
+    disk.history(name, category, decision, tax)
+
+
 def main():
     print('Welcome to Shedlia\'s Wheels 4 You')
     answer = ''
@@ -91,32 +107,45 @@ def main():
         else:
             print('please choose one or the other and spell it correctly...')
 
+
 def employee_main():
     name = input('What is your name?\n\t:').title().strip()
     print('Welcome {}!!!'.format(name))
     decision = ''
     while decision != 'q':
-        decision = input('What do you want to do today?\n\t1. Add product to stock\n\t2. Check total revenue\n\t3.See transaction History\n\t**Pick numbers Only**\n**Choose ''q'' to quit at any time**\n:')
+        decision = input(
+            'What do you want to do today?\n\t1. Add product to stock\n\t2. Check total revenue\n\t3.See transaction History\n\t**Pick numbers Only**\n**Choose '
+            'q'
+            ' to quit at any time**\n:')
         if decision == '1':
-            category = input('What is the name of the item you want to rent out?\n**How you type it is how it will show in the inventory**\n:')
-            original_price = float(input('what is the price you payed for one of that particular item?\n:'))
+            category = input(
+                'What is the name of the item you want to rent out?\n**How you type it is how it will show in the inventory**\n:'
+            )
+            original_price = float(
+                input(
+                    'what is the price you payed for one of that particular item?\n:'
+                ))
             stock = int(input('How many of them do you have currently?\n:'))
-            rental_price = float(input('What do you want the rental price to be?\n:'))
-            add_new_product(category,original_price,stock,rental_price)
-            print('The item {} with the rental price of ${:.2f} has been added to the inventory'.format(category, rental_price))
+            rental_price = float(
+                input('What do you want the rental price to be?\n:'))
+            disk.add_new_product(category, original_price, stock, rental_price)
+            print(
+                'The item {} with the rental price of ${:.2f} has been added to the inventory'.
+                format(category, rental_price))
         elif decision == '2':
-            total = total_revenue_reader()
-            revenue = total_revenue(total)
+            total = disk.total_revenue_reader()
+            revenue = core.total_revenue(total)
             print('Your total revenue would be:\n${:.2f}\n'.format(revenue))
 
         elif decision == 'q':
             print('Have a nice day {}!!!'.format(name))
         elif decision == '3':
-            print('These are all your transactions:\n\n',history_reader(), sep = '')
-            
+            print('These are all your transactions:\n\n{}'.format(
+                disk.history_reader()))
+
         else:
             print('Not Valid Input... Please Try Again!!!\n')
-            
+
+
 if __name__ == '__main__':
     main()
-
--- test_core.py
+++ test_core.py
@@ -1,29 +1,94 @@
 from core import *
 
+
 def test_remove_one_atv():
-    assert (remove_stock('Atv', {'Atv':{'price': 30, 'stock': 5}}) ==
-            {'Atv': {'price': 30, 'stock': 4}})
+    assert (remove_stock('Atv', {'Atv': {
+        'price': 30,
+        'stock': 5
+    }}) == {
+        'Atv': {
+            'price': 30,
+            'stock': 4
+        }
+    })
+
 
 def test_remove_atv_with_more_than_one_dict():
-    assert (remove_stock('Atv', {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price': 100, 'stock': 5}}) ==
-            {'Atv':{'price': 30, 'stock': 4}, 'Side-by-Side':{'price':100, 'stock': 5}})
+    assert (remove_stock('Atv', {
+        'Atv': {
+            'price': 30,
+            'stock': 5
+        },
+        'Side-by-Side': {
+            'price': 100,
+            'stock': 5
+        }
+    }) == {
+        'Atv': {
+            'price': 30,
+            'stock': 4
+        },
+        'Side-by-Side': {
+            'price': 100,
+            'stock': 5
+        }
+    })
+
 
 def test_remove_Bike():
-    assert (remove_stock('Bike', {'Bike':{'stock':5}}) == 
-            {'Bike': {'stock': 4}})
+    assert (remove_stock('Bike', {'Bike': {
+        'stock': 5
+    }}) == {
+        'Bike': {
+            'stock': 4
+        }
+    })
+
 
 def test_remove_Scooter():
-    assert (remove_stock('Scooter', {'Scooter':{'stock':5}}) == 
-            {'Scooter': {'stock': 4}})
+    assert (remove_stock('Scooter', {'Scooter': {
+        'stock': 5
+    }}) == {
+        'Scooter': {
+            'stock': 4
+        }
+    })
 
 
 def test_remove_one_sbs():
-    assert (remove_stock('Side-by-Side', {'Side-by-Side':{'price': 30, 'stock': 5}}) ==
-            {'Side-by-Side': {'price': 30, 'stock': 4}})
+    assert (remove_stock('Side-by-Side',
+                         {'Side-by-Side': {
+                             'price': 30,
+                             'stock': 5
+                         }}) == {
+                             'Side-by-Side': {
+                                 'price': 30,
+                                 'stock': 4
+                             }
+                         })
+
 
 def test_remove_sbs_with_more_than_one_dict():
-    assert (remove_stock('Side-by-Side', {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price': 100, 'stock': 5}}) ==
-            {'Atv':{'price': 30, 'stock': 5}, 'Side-by-Side':{'price':100, 'stock': 4}})
+    assert (remove_stock('Side-by-Side', {
+        'Atv': {
+            'price': 30,
+            'stock': 5
+        },
+        'Side-by-Side': {
+            'price': 100,
+            'stock': 5
+        }
+    }) == {
+        'Atv': {
+            'price': 30,
+            'stock': 5
+        },
+        'Side-by-Side': {
+            'price': 100,
+            'stock': 4
+        }
+    })
+
 
 def test_sales_tax():
     assert sales_tax(3) == 3.21
@@ -31,32 +96,67 @@ def test_sales_tax():
     assert sales_tax(1) == 1.07
     assert sales_tax(0) == 0
 
+
 def test_total_for_one_atv():
-    assert total(1, 'Atv',{'Atv':{'rental price': 30, 'stock': 5}}) == 30
+    assert total(1, 'Atv', {'Atv': {'rental price': 30, 'stock': 5}}) == 30
+
 
 def test_total_for_two_atvs():
-    assert total(2,'Atv', {'Atv':{'rental price': 30, 'stock': 5}}) == 60
+    assert total(2, 'Atv', {'Atv': {'rental price': 30, 'stock': 5}}) == 60
+
 
 def test_add_atv():
-    assert (add_stock('Atv', {'Atv':{'price': 30, 'stock': 5}}) ==
-            {'Atv': {'price': 30, 'stock': 6}})
+    assert (add_stock('Atv', {'Atv': {
+        'price': 30,
+        'stock': 5
+    }}) == {
+        'Atv': {
+            'price': 30,
+            'stock': 6
+        }
+    })
+
 
 def test_add_sbs():
-    assert (add_stock('Side-by-Side', {'Side-by-Side':{'price': 30, 'stock': 5}}) ==
-            {'Side-by-Side': {'price': 30, 'stock': 6}})
+    assert (add_stock('Side-by-Side',
+                      {'Side-by-Side': {
+                          'price': 30,
+                          'stock': 5
+                      }}) == {
+                          'Side-by-Side': {
+                              'price': 30,
+                              'stock': 6
+                          }
+                      })
+
 
 def test_add_Scooter():
-    assert (add_stock('Scooter', {'Scooter':{'price': 30, 'stock': 5}}) ==
-            {'Scooter': {'price': 30, 'stock': 6}})
+    assert (add_stock('Scooter', {'Scooter': {
+        'price': 30,
+        'stock': 5
+    }}) == {
+        'Scooter': {
+            'price': 30,
+            'stock': 6
+        }
+    })
+
 
 def test_add_Bike():
-    assert (add_stock('Bike', {'Bike':{'price': 30, 'stock': 5}}) ==
-            {'Bike': {'price': 30, 'stock': 6}})
+    assert (add_stock('Bike', {'Bike': {
+        'price': 30,
+        'stock': 5
+    }}) == {
+        'Bike': {
+            'price': 30,
+            'stock': 6
+        }
+    })
+
 
 def test_deposit():
-    assert (deposit('Bike', {'Bike': {'Original Price': 350.0}}) ==
-            35.0)
+    assert (deposit('Bike', {'Bike': {'Original Price': 350.0}}) == 35.0)
+
 
 def test_total_revenue():
-    assert total_revenue([30.0,60.0,10.0]) == 100.0
-
+    assert total_revenue([30.0, 60.0, 10.0]) == 100.0
