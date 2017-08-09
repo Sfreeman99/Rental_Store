8c635956cd16a10de0a28ae29d2ffaee413f9c05
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-08-09 11:49:29 -0500

    organized my shell better

--- shell.py
+++ shell.py
@@ -2,6 +2,60 @@ import core
 import disk
 
 
+def main():
+    print('Welcome to Shedlia\'s Wheels 4 U')
+    answer = ''
+    while True:
+        answer = input('Are you a customer or employee\n:').lower().strip()
+        if answer == 'customer':
+            customer_main()
+            exit()
+        elif answer == 'employee':
+            employee_main()
+            exit()
+        else:
+            print('please choose one or the other and spell it correctly...')
+
+
+def employee_main():
+    name = input('What is your name?\n\t:').title().strip()
+    print('Welcome {}!!!'.format(name))
+    decision = ''
+    while decision != 'q':
+        decision = input(
+            'What do you want to do today?\n\t1. Add product to stock\n\t2. Check total revenue\n\t3.See transaction History\n\t**Pick numbers Only**\n**Choose '
+            'q'
+            ' to quit at any time**\n:')
+        if decision == '1':
+            category = input(
+                'What is the name of the item you want to rent out?\n**How you type it is how it will show in the inventory**\n:'
+            )
+            original_price = float(
+                input(
+                    'what is the price you payed for one of that particular item?\n:'
+                ))
+            stock = int(input('How many of them do you have currently?\n:'))
+            rental_price = float(
+                input('What do you want the rental price to be?\n:'))
+            disk.add_new_product(category, original_price, stock, rental_price)
+            print(
+                'The item {} with the rental price of ${:.2f} has been added to the inventory'.
+                format(category, rental_price))
+        elif decision == '2':
+            total = disk.total_revenue_reader()
+            revenue = core.total_revenue(total)
+            print('Your total revenue would be:\n${:.2f}\n'.format(revenue))
+
+        elif decision == 'q':
+            print('Have a nice day {}!!!'.format(name))
+        elif decision == '3':
+            print('These are all your transactions:\n\n{}'.format(
+                disk.history_reader()))
+
+        else:
+            print('Not Valid Input... Please Try Again!!!\n')
+
+
 def customer_main():
     print('Welcome Customer')
     decision = ''
@@ -51,9 +105,23 @@ def rent():
     disk.history(name, category, decision, tax)
 
 
+def return_item():
+    decision = 'return'
+    rental_products = disk.read_inventory()
+    current_menu = disk.menu(rental_products)
+    name = input('What is your name?\n:').title().strip()
+    category = category_decision(decision, current_menu)
+    new_inventory = core.add_stock(category, rental_products)
+    disk.write_inventory(new_inventory)
+    current_deposit = core.deposit(category, rental_products)
+    print('Here is your deposit of ${:.2f} back {}.\n\tHave A Nice Day !!!'.
+          format(current_deposit, name))
+    disk.return_history(name, category, decision, current_deposit)
+
+
 def how_many_hours_decision(category, name):
     how_long = ''
-    while True:
+    while how_long != '1' or '2' or '3' or '4':
         how_long = input(
             'How long would you like to rent the {} {}\n\t1. 1hr\n\t2. 2hrs\n\t3. 3hrs\n\t4. 4hrs?\n:'.
             format(category, name))
@@ -79,74 +147,6 @@ def category_decision(decision, current_menu):
             break
         else:
             print('Invalid Choice.... Please Try Again\n')
-
-
-def return_item():
-    decision = 'return'
-    rental_products = disk.read_inventory()
-    current_menu = disk.menu(rental_products)
-    name = input('What is your name?\n:').title().strip()
-    category = category_decision(decision, current_menu)
-    new_inventory = core.add_stock(category, rental_products)
-    disk.write_inventory(new_inventory)
-    current_deposit = core.deposit(category, rental_products)
-    print('Here is your deposit of ${:.2f} back {}.\n\tHave A Nice Day !!!'.
-          format(current_deposit, name))
-    disk.return_history(name, category, decision, current_deposit)
-
-
-def main():
-    print('Welcome to Shedlia\'s Wheels 4 You')
-    answer = ''
-    while answer != 'customer' or 'employee':
-        answer = input('Are you a customer or employee\n:').lower().strip()
-        if answer == 'customer':
-            customer_main()
-            exit()
-        elif answer == 'employee':
-            employee_main()
-            exit()
-        else:
-            print('please choose one or the other and spell it correctly...')
-
-
-def employee_main():
-    name = input('What is your name?\n\t:').title().strip()
-    print('Welcome {}!!!'.format(name))
-    decision = ''
-    while decision != 'q':
-        decision = input(
-            'What do you want to do today?\n\t1. Add product to stock\n\t2. Check total revenue\n\t3.See transaction History\n\t**Pick numbers Only**\n**Choose '
-            'q'
-            ' to quit at any time**\n:')
-        if decision == '1':
-            category = input(
-                'What is the name of the item you want to rent out?\n**How you type it is how it will show in the inventory**\n:'
-            )
-            original_price = float(
-                input(
-                    'what is the price you payed for one of that particular item?\n:'
-                ))
-            stock = int(input('How many of them do you have currently?\n:'))
-            rental_price = float(
-                input('What do you want the rental price to be?\n:'))
-            disk.add_new_product(category, original_price, stock, rental_price)
-            print(
-                'The item {} with the rental price of ${:.2f} has been added to the inventory'.
-                format(category, rental_price))
-        elif decision == '2':
-            total = disk.total_revenue_reader()
-            revenue = core.total_revenue(total)
-            print('Your total revenue would be:\n${:.2f}\n'.format(revenue))
-
-        elif decision == 'q':
-            print('Have a nice day {}!!!'.format(name))
-        elif decision == '3':
-            print('These are all your transactions:\n\n{}'.format(
-                disk.history_reader()))
-
-        else:
-            print('Not Valid Input... Please Try Again!!!\n')
 
 
 if __name__ == '__main__':
